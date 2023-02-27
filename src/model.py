d6a9c1e56c21395723cb51e8e01dd8a183d45af5
13Noodles <13noodles@protonmail.com>
2023-02-28 00:23:01 +0100

    completed issue #1 tasks 7 to 14 (linked model w/ fileRepo & added Point+temps data)

--- src/model.py
+++ src/model.py
@@ -1,3 +1,24 @@
+from fileRepo import FileRepo
+from point import Point
+
 class Model:
-    def __init__(self):
-        pass
+    def __init__(self, defaultSaveDir:str="saves"):
+        self.__points = []
+        self.__temps = []
+        self.__saveDir = defaultSaveDir
+        self.fileRepo = FileRepo()
+
+    def getPoint(self, pointId: int) -> Point:
+        if(pointId<0 or pointId>len(self.__points)-1):
+            return None
+        return self.__points[pointId]
+
+    def getTemps(self, tempsId: int) -> int:
+        if(tempsId<0 or tempsId>len(self.__temps)-1):
+            return None
+        return self.__points[pointId]
+
+    def exportToCSV(self, filename: str):
+        savePath = self.__saveDir+"/"+filename
+        self.fileRepo.transformDataToCSV(self.__points,self.__temps,savePath)
+

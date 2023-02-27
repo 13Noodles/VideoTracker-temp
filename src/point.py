270f89994f20ecc24046ffcab770e8cc5e7b33e0
13Noodles <13noodles@protonmail.com>
2023-02-27 23:53:05 +0100

    added Point (tasks 1 to 4, issue #1)

---
+++ src/point.py
@@ -0,0 +1,12 @@
+
+class Point:
+    def __init__(self,x:float, y:float):
+        self.__x = x
+        self.__y = y
+
+    def getX(self) -> float:
+        return self.__x
+
+    def getY(self) -> float:
+        return self.__y
+
